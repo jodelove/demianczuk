ad5a61396f705dc158a330c216f9d87538ac1b95
malps13 <m13@balloonstudio.pl>
2015-09-22 23:15:52 +0200

    Add subscribe push menu

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Demiańczuk - edukacja matematyczna' });
+});
+
+router.get('/o-mnie', function(req, res, next) {
+  res.render('about-me', { title: 'Demiańczuk - o firmie' });
+});
+
+router.get('/oferta', function(req, res, next) {
+  res.render('offer', { title: 'Demiańczuk - oferta edukacyjna' });
+});
+
+router.get('/referencje', function(req, res, next) {
+  res.render('testimonials', { title: 'Demiańczuk - referencje' });
+});
+
+router.get('/kontakt', function(req, res, next) {
+  res.render('contact', { title: 'Demiańczuk - informacje kontaktowe' });
 });
 
 module.exports = router;
