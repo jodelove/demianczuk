1d59e6873cc5bf42811494c6728e6a93c9ae65cf
malps13 <m13@balloonstudio.pl>
2015-11-16 00:20:50 +0100

    Add toasts, show errors in subscribe forms and style testimonials and contact page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,7 +2,12 @@ var app = angular.module('demianczuk', [
   'ui.router', 
   'ct.ui.router.extras.dsr', 
   'ct.ui.router.extras.sticky',
-  'ngAnimate'
+  'ngAnimate',
+  'ui.bootstrap.showErrors',
+  'elif',
+  'ngSanitize',
+  'ngToast',
+  'ui.validate'
 ]);
 
 app.run(function ($rootScope, $state) {
@@ -13,6 +18,7 @@ app.run(function ($rootScope, $state) {
 
 function SidebarService() {
   var sidebar = false;
+  var isDarkNavbar = true;
 
   function openSidebar() {
     sidebar = true;
@@ -22,11 +28,24 @@ function SidebarService() {
     sidebar = false;
   }
 
+  function darkNavbar() {
+    isDarkNavbar = true;
+  }
+
+  function brightNavbar() {
+    isDarkNavbar = false;
+  }
+
   return {
     open: openSidebar,
     close: closeSidebar,
+    darkNavbar: darkNavbar,
+    brightNavbar: brightNavbar,
     isOpened: function () {
       return sidebar;
+    },
+    isDarkNavbar: function () {
+      return isDarkNavbar;
     }
   };
 }
@@ -35,8 +54,78 @@ app.factory('sidebarService', function () {
   return new SidebarService();
 });
 
-app.controller('SidebarCtrl', function ($scope, $state, sidebarService) {
+app.controller('SidebarCtrl', function ($scope, $state, $timeout, sidebarService, ngToast) {
   $scope.sidebar = sidebarService;
+  $scope.spinner = false;
+
+  $scope.sending = false;
+  $scope.subscription = {};
+
+  $scope.validateContact = function (value) {
+    var phoneRegexp = /^[\s()+-]*([0-9][\s()+-]*){6,20}$/;
+    var emailRegexp = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    if (!phoneRegexp.test(value) && !emailRegexp.test(value)) {
+      return false;
+    }
+    return true;
+  };
+
+  $scope.courseOptions = [
+    'Egzamin gimnazjalny',
+    'Matura podstawowa',
+    'Matura rozszerzona',
+    'Student',
+    'Lekcje indywidualne'
+  ];
+
+  $scope.submitForm = function (form) {
+    $scope.$broadcast('show-errors-check-validity');
+
+    if (form.$valid) {
+      $scope.sending = true;
+      
+      $timeout(function () {
+        $scope.$broadcast('show-errors-reset');
+        $timeout(function () {
+          $scope.subscription = {};
+          $scope.subscriptionForm.$setPristine();
+          $scope.subscriptionForm.$setUntouched();
+          $scope.sending = false;
+          $timeout(function () {
+            ngToast.create({
+              content: 'Dziękuje za wiadomość, skontaktuję się z Tobą w ciągu jednego dnia.',
+              dismissButton: true
+            });
+          }, 0);
+          $state.go('pages');
+        });
+      }, 1000);
+    }
+  }
+
+  $scope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+    //console.log(fromState);
+    //console.log(toState);
+    if (toState && (toState.name === 'pages.testimonials' || toState.name === 'pages.contact')) {
+      $scope.sidebar.brightNavbar();
+    } else  {
+      $scope.sidebar.darkNavbar();
+    }
+
+    if (fromState != 'subscribe' && (toState.name === 'pages.home' || toState.name === 'pages.offer' || toState.name === 'pages.testimonials' || toState.name === 'pages.contact')) {
+      //$scope.spinner = true;
+    }
+  });
+
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+    if (toState != 'subscribe' && fromState != 'subscribe') {
+      $("html, body").animate({ scrollTop: 0 }, 0);
+    }
+
+    $timeout(function () {
+      $scope.spinner = false;
+    }, 1000);
+  });
 });
 
 // offer controller
@@ -47,13 +136,7 @@ app.controller('OfferCtrl', function ($scope) {
 
 // configure application
 
-app.config(function($stateProvider, $urlRouterProvider, $locationProvider, $provide) {
-  /*$provide.decorator('$uiViewScroll', function ($delegate) {
-    return function (uiViewElement) {
-      window.scrollTo(0, 0);
-    }; 
-  });*/
-
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider, $provide, ngToastProvider) {
   $urlRouterProvider.otherwise('/');
   $stateProvider
     .state('pages', {
@@ -121,4 +204,143 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider, $prov
     enabled: true,
     requireBase: false
   });
+
+  ngToastProvider.configure({
+    animation: 'slide'
+  });
+});
+
+app.directive('loading-button', function() {
+  return {
+    replace: true,
+    transclude: true,
+    restrict: 'E',
+    link: function (scope, rocketContext, attrs) {
+      scope.size = attrs.size || 40;
+
+      var rocket = rocketContext[0].getElementsByClassName('preloader-rocket-wrapper')[0];
+
+      var flyIn = function() {
+        setTimeout(function() {
+          rocket.setAttribute('class', 'preloader-rocket-wrapper');
+          setTimeout(function() {
+            rocket.setAttribute('class', 'preloader-rocket-wrapper animate');
+          }, 450);
+        }, 50);
+      };
+
+      var flyOut = function (callback) {
+        rocket.setAttribute('class', 'preloader-rocket-wrapper flyover animate');
+        setTimeout(function() {
+          rocket.setAttribute('class', 'preloader-rocket-wrapper flyover');
+          if (callback) {
+            callback();
+          }
+        }, 1000);
+      };
+
+      scope.$watch('loading', function (cur, prev) {
+        if(prev == cur) return;
+        if(cur === true) {
+          flyIn();
+        }
+      });
+
+      scope.$watch('finish', function (cur, prev) {
+        if(prev == cur) return;
+        if(cur === true) {
+          flyOut(function() {
+            scope.afterFinish();
+          });
+        }
+      });
+      
+      if('started' in attrs)
+        flyIn();
+    },
+    controller: function ($scope, $q, $timeout) {
+      var outTimeout;
+
+      $scope.startRun = function() {
+        $timeout.cancel(outTimeout);
+        $scope.longRunButton.loading = true;
+        $scope.longRunButton.loadingVisible = true;
+        $scope.longRunButton.loadingFinish = false;
+
+        if(typeof $scope.running !== 'undefined') {
+          $scope.running = true;
+        }
+        if(typeof $scope.runningAnimation !== 'undefined') {
+          $scope.runningAnimation = true;
+        }
+
+        var promise = $scope.run();
+
+        var estimated = (new Date()).getTime() + 500;
+
+        $scope.longRunButton.afterRocketFlyOut = function() {
+          $timeout(function() {
+            $scope.longRunButton.loadingVisible = false;
+            if($scope.runningAnimation) {
+              $scope.runningAnimation = false;
+            }
+            $scope.onFinishAnimation();
+          }, 5);
+        };
+
+        var promiseHandler = function(onFinish) {
+          var func = function(data) {
+            var currentTime = (new Date()).getTime();
+            if (currentTime < estimated) {
+              $timeout(function() {
+                func(data);
+              }, estimated - currentTime);
+              return;
+            }
+
+            $timeout(function() {
+              $scope.longRunButton.loadingFinish = true;
+            }, 10);
+
+            outTimeout = $timeout(function() {
+              $scope.longRunButton.loading = false;
+              if ($scope.running) {
+                $scope.running = false;
+              }
+            }, 330);
+            onFinish(data);
+          };
+          return func;
+        };
+
+        $q.when(promise).then(promiseHandler(function(data) {
+          $scope.onFinish(data);
+        })).catch(function(err) {
+          
+          $scope.longRunButton.loadingFinish = true;
+          $scope.longRunButton.loading = false;
+          $scope.longRunButton.loadingVisible = false;
+          if($scope.runningAnimation) {
+            $scope.runningAnimation = false;
+          }
+          if($scope.running) {
+            $scope.running = false;
+          }
+          if($scope.$parent) {
+            $scope.$parent.$error = err;
+          }
+          $scope.onError(err);
+        });
+      };
+    },
+    scope: {
+      running: '=',
+      runningAnimation: '=',
+      onError: '&',
+      onFinish: '&',
+      onFinishAnimation: '&',
+      run: '&'
+    },
+    template: require('./loadingButton.jade!')()
+  }
 });
