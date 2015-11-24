8801004ada2a4042271b160969128912671fcdb0
malps13 <m13@balloonstudio.pl>
2015-11-24 12:46:19 +0100

    install all integrations at once

--- public/javascripts/ux.js
+++ public/javascripts/ux.js
@@ -1,1 +1,35 @@
+// lucky orange
+window.__wtw_lucky_site_id = 46951;
+(function() {
+  var wa = document.createElement('script'); wa.type = 'text/javascript'; wa.async = true;
+  wa.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://cdn') + '.luckyorange.com/w.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
+})();
+
+// usability tools
 (function(u, t, c, s, r) { !t[c] && (t[c] = { projectId: '56543b15e4b05a688e3b6efa'}) && (r = u.createElement(s)) && (r.src = 'https://mobile.usabilitytools.com/recorder/tracker/tracker.js') && (u.head.appendChild(r)); })(document, window, 'UsabilityTools Session Recording', 'script');
+
+// hotjar
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:108751,hjsv:5};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+
+// fullstory
+
+window['_fs_debug'] = false;
+window['_fs_host'] = 'www.fullstory.com';
+window['_fs_org'] = 'N94Y';
+(function(m,n,e,t,l,o,g,y){
+  g=m[e]=function(a,b){g.q?g.q.push([a,b]):g._api(a,b);};g.q=[];
+  o=n.createElement(t);o.async=1;o.src='https://'+_fs_host+'/s/fs.js';
+  y=n.getElementsByTagName(t)[0];y.parentNode.insertBefore(o,y);
+  g.identify=function(i,v){g(l,{uid:i});if(v)g(l,v)};g.setUserVars=function(v){FS(l,v)};
+  g.identifyAccount=function(i,v){o='account';v=v||{};v.acctId=i;FS(o,v)};
+  g.clearUserCookie=function(d,i){d=n.domain;while(1){n.cookie='fs_uid=;domain='+d+
+  ';path=/;expires='+new Date(0);i=d.indexOf('.');if(i<0)break;d=d.slice(i+1)}}
+})(window,document,'FS','script','user');
