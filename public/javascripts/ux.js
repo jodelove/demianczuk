0426d90d37bd36e409c5d70aa5d4645f47cc20b0
malps13 <m13@balloonstudio.pl>
2015-11-24 12:20:14 +0100

    fix lucky orange integration

---
+++ public/javascripts/ux.js
@@ -0,0 +1,7 @@
+window.__wtw_lucky_site_id = 46951;
+
+(function() {
+  var wa = document.createElement('script'); wa.type = 'text/javascript'; wa.async = true;
+  wa.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://cdn') + '.luckyorange.com/w.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
+})();
