1a81d2084559f86d272cfda33c54d97ed9bc8a22
malps13 <m13@balloonstudio.pl>
2016-08-29 23:47:03 +0200

    Replace mock opinions with real ones

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,9 @@ var router = express.Router();
 var apiKey = process.env.DEMIANCZUK_MAILGUN_API_KEY;
 var domain = 'demianczuk.edu.pl';
 var mailgun = require('mailgun-js')({apiKey: apiKey, domain: domain});
+var syncRequest = require('sync-request');
+var cheerio = require('cheerio');
+var polishPlural = require('smart-plurals').Plurals.getRule('pl');
 
 var app = express();
 
@@ -43,24 +46,68 @@ router.get('/wyslij-mail', function (req, res, next) {
 });
 
 router.get('/opinie', function (req, res, next) {
-  // call to http://www.e-korepetycje.net/bartosz-demianczuk/matematyka
+  var email = app.get('env') === 'development' ? 'demianczuk@mailinator.com' : 'bartosz.demianczuk@gmail.com';
+  var opinionsHtml = syncRequest('GET', 'http://www.e-korepetycje.net/bartosz-demianczuk#opinie');
+
   res.setHeader('Content-Type', 'application/json');
+
+  var failure = function () {
+    res.status(500);
+    return res.send(JSON.stringify({
+      success: false,
+      error: "Nie udało się pobrać opinii"
+    }));
+  }
+
+  // check if we got HTML with opinions
+  if (!opinionsHtml || opinionsHtml.statusCode !== 200) {
+    failure();
+  }
+
+  // try to parse opinions
+  var $  = cheerio.load(opinionsHtml.getBody());
+  var stars = $('.opinion-list .opinion .stars');
+
+  if (!stars.length) {
+    failure();
+  }
+
+  var totalScore = 0;
+
+  for (var i = 0; i < stars.length; i++) {
+    var star = stars[i];
+    var score = parseInt($(stars[i]).prop('class').replace('stars', '').replace('small', '').replace('s_', '').trim()) * 2;
+    totalScore += score;
+  }
+  totalScore = totalScore / (stars.length * 100) * 5.0;
+
+  var scoreStars = [];
+  for (var i = 0, score = totalScore; i < 5; i++) {
+    if (score > 1) {
+      scoreStars.push('fa-star');
+      score -= 1.0;
+    } else if (score > 0.75) {
+      scoreStars.push('fa-star');
+      score -= 0.75;
+    } else if (score > 0.50) {
+      scoreStars.push('fa-star-half-o');
+      score -= 0.50;
+    } else if (score > 0.25) {
+      scoreStars.push('fa-star-half-o');
+      score -= 0.25;
+    } else {
+      scoreStars.push('fa-star-o');
+    }
+  }
+
   return res.send(JSON.stringify({
     success: true,
     stats: {
-      count: 1,
-      opinionCountString: '1 opinię',
-      scoreString: '5.0',
-      scoreStars: ['fa-star', 'fa-star', 'fa-star', 'fa-star', 'fa-star']
-    },
-    opinions: [
-      {
-        name: "Michalina",
-        time: "18 sierpnia 2016",
-        score: 100,
-        textBody: "<p>Korepetytor godny polecenia, udziela cennych rad.</p>"
-      }
-    ]
+      count: stars.length,
+      opinionCountString: stars.length +  " " + polishPlural(stars.length, ['opinia', 'opinie', 'opinii']),
+      scoreString: totalScore.toFixed(2),
+      scoreStars: scoreStars
+    }
   }))
 });
 
